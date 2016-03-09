887f7a263266e882da138e2382166ec33c25606f
Sebastian Larsson <sebastian.larsson@cybercom.com>
2016-03-09 10:50:52 +0100

    red multiple_cards

--- src/test/java/CardsGameTest.java
+++ src/test/java/CardsGameTest.java
@@ -19,8 +19,12 @@ public class CardsGameTest {
 		assertEquals("p1 wins 1 to 0", gameResult(hand('3'), hand('2')));
 	}
 
+	@Test
+	public void multiple_cards() throws Exception {
+		assertEquals("p1 wins 2 to 0", gameResult(hand('3','3'), hand('2','2')));
+	}
+
 	private String gameResult(char[] p1Cards, char[] p2Cards) {
-		// TODO Auto-generated method stub
 		if(p1Cards[0] > p2Cards[0])
 			return "p1 wins 1 to 0";
 		return "draw";
