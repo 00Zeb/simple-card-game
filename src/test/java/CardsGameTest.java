df814dffa59194e001f2d6c66bc9e05e7d39b1ab
Sebastian Larsson <sebastian.larsson@cybercom.com>
2016-03-09 10:45:12 +0100

    red draw

--- src/test/java/CardsGameTest.java
+++ src/test/java/CardsGameTest.java
@@ -1,4 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 
 public class CardsGameTest {
 
+	@Test
+	public void draw() throws Exception {
+		assertEquals("draw", gameResult(new char[] {'2'}, new char[] {'2'}));
+	}
+
+	private Object gameResult(char[] cs, char[] cs2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
