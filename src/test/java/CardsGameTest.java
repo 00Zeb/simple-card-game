2c9073c5f8f8d54e630be1b90d202aee69c03fb4
Sebastian Larsson <sebastian.larsson@cybercom.com>
2016-03-09 10:49:51 +0100

    refactoring

--- src/test/java/CardsGameTest.java
+++ src/test/java/CardsGameTest.java
@@ -7,12 +7,16 @@ public class CardsGameTest {
 
 	@Test
 	public void draw() throws Exception {
-		assertEquals("draw", gameResult(new char[] {'2'}, new char[] {'2'}));
+		assertEquals("draw", gameResult(hand('2'), hand('2')));
+	}
+
+	private char[] hand(char... values) {
+		return values;
 	}
 
 	@Test
 	public void single_card() throws Exception {
-		assertEquals("p1 wins 1 to 0", gameResult(new char[] {'3'}, new char[] {'2'}));
+		assertEquals("p1 wins 1 to 0", gameResult(hand('3'), hand('2')));
 	}
 
 	private String gameResult(char[] p1Cards, char[] p2Cards) {
