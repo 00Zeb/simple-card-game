e1951c63a017285acac4121b90dab22369daf7e4
Sebastian Larsson <sebastian.david.larsson@gmail.com>
2026-09-07 20:54:17 +0200

    green: tied cards award no points

--- src/cardsGame.js
+++ src/cardsGame.js
@@ -2,6 +2,9 @@ const RANK = { A: 14, K: 13 };
 
 module.exports = (steve, josh) => {
   if (steve.length === 0) return 'Tie';
-  if (RANK[steve[0]] > RANK[josh[0]]) return 'Steve wins 1 to 0';
-  return 'Josh wins 0 to 1';
+  const s = RANK[steve[0]];
+  const j = RANK[josh[0]];
+  if (s > j) return 'Steve wins 1 to 0';
+  if (s < j) return 'Josh wins 0 to 1';
+  return 'Tie';
 };
