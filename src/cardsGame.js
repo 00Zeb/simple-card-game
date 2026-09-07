1efbb5521df824e05e5758f380092c43d0402ce7
Sebastian Larsson <sebastian.david.larsson@gmail.com>
2026-09-07 20:54:05 +0200

    green: josh wins when his card is higher

--- src/cardsGame.js
+++ src/cardsGame.js
@@ -3,5 +3,5 @@ const RANK = { A: 14, K: 13 };
 module.exports = (steve, josh) => {
   if (steve.length === 0) return 'Tie';
   if (RANK[steve[0]] > RANK[josh[0]]) return 'Steve wins 1 to 0';
-  return 'Tie';
+  return 'Josh wins 0 to 1';
 };
