2085ea2108961de7138ca30e14eca73d1e95a828
Sebastian Larsson <sebastian.david.larsson@gmail.com>
2026-09-12 23:14:08 +0200

    red: award the round to Steve when his card is higher

--- src/cardsGame.test.js
+++ src/cardsGame.test.js
@@ -4,4 +4,8 @@ describe('cardsGame', () => {
   it('reports a tie when both decks are empty', () => {
     expect(cardsGame([], [])).toBe('Tie');
   });
+
+  it('awards the round to Steve when his card is higher', () => {
+    expect(cardsGame(['A'], ['K'])).toBe('Steve wins 1 to 0');
+  });
 });
