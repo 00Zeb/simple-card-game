7032be101d0c315896c11b6670f4781dd2e7c12f
Sebastian Larsson <sebastian.david.larsson@gmail.com>
2026-09-12 23:16:00 +0200

    red: award the round to Josh when his card is higher

--- src/cardsGame.test.js
+++ src/cardsGame.test.js
@@ -8,4 +8,8 @@ describe('cardsGame', () => {
   it('awards the round to Steve when his card is higher', () => {
     expect(cardsGame(['A'], ['K'])).toBe('Steve wins 1 to 0');
   });
+
+  it('awards the round to Josh when his card is higher', () => {
+    expect(cardsGame(['K'], ['A'])).toBe('Josh wins 1 to 0');
+  });
 });
