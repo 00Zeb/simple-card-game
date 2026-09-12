188e7863ed8b12cb88ca74efb54bd5f87def2acc
Sebastian Larsson <sebastian.david.larsson@gmail.com>
2026-09-12 23:12:57 +0200

    red: tie when both decks are empty

--- src/cardsGame.test.js
+++ src/cardsGame.test.js
@@ -1,7 +1,7 @@
 const cardsGame = require('./cardsGame');
 
 describe('cardsGame', () => {
-  it('empty', () => {
+  it('reports a tie when both decks are empty', () => {
+    expect(cardsGame([], [])).toBe('Tie');
   });
-
 });
